28b01617b1f7f1c91177c4f246542cec45ccf826
Tomasz Suski <tomasz.suski@orba.co>
2024-05-07 20:55:20 +0200

    adding Person to db via form executed

--- src/main/java/com/lingarogroup/peopledbweb/web/controller/PeopleController.java
+++ src/main/java/com/lingarogroup/peopledbweb/web/controller/PeopleController.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 
@@ -22,8 +23,21 @@ public class PeopleController {
     public Iterable<Person> getPeople() {
         return personRepository.findAll();
     }
+
+    @ModelAttribute
+    public Person getPerson() {
+        return new Person();
+    }
     @GetMapping
     public String showPeoplePage() {
         return "people";
+    }
+
+    @PostMapping
+    public String savePerson(Person person) {
+        personRepository.save(person);
+//        redirect: is a special prefix that tells Spring MVC to redirect to a different route.
+//        in this case route is the same, but it refreshes the page
+        return "redirect:people";
     }
 }
--- src/main/java/com/lingarogroup/peopledbweb/web/formatter/LocalDateFormatter.java
+++ src/main/java/com/lingarogroup/peopledbweb/web/formatter/LocalDateFormatter.java
@@ -11,15 +11,16 @@ import java.util.Locale;
 @Component
 public class LocalDateFormatter implements Formatter<LocalDate> {
 
-    private final DateTimeFormatter formatter = DateTimeFormatter.ofPattern("MMMM dd, yyyy");
+    private final DateTimeFormatter printFormatter = DateTimeFormatter.ofPattern("MMMM dd, yyyy");
+    private final DateTimeFormatter parseFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
 
     @Override
     public LocalDate parse(String text, Locale locale) throws ParseException {
-        return LocalDate.parse(text, formatter);
+        return LocalDate.parse(text, parseFormatter);
     }
 
     @Override
     public String print(LocalDate object, Locale locale) {
-        return formatter.format(object);
+        return printFormatter.format(object);
     }
 }
